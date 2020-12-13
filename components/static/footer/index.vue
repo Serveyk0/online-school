4bda67a311177e3cdabaf03f5f809a4d3252186e
Serveyk0 <ssimpl8ss@gmail.com>
2020-12-13 18:18:46 +0200

    initial commit

---
+++ components/static/footer/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <footer class="footer">
+        footer
+    </footer>
+</template> 
+
+<style lang="sass" scoped>
+ @import '~~/assets/sass/footer'
+</style>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
---
+++ components/static/header/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <header class="header">
+        Header
+    </header>
+</template> 
+
+<style lang="sass" scoped>
+ @import '~~/assets/sass/header'
+</style>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
