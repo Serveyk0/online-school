1972621c0051dc4fb93b198a43c4e5309d10660e
Serveyk0 <ssimpl8ss@gmail.com>
2021-01-16 16:32:26 +0200

    1

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,24 +1,14 @@
-const imageminMozjpeg = require('imagemin-mozjpeg')
-const ImageminPlugin = require('imagemin-webpack-plugin').default
-const isDev = process.env.NODE_ENV !== 'production'
+export default {
+  // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
+  ssr: true,
 
-module.exports = {
-  target: 'me_static',
   buildDir: "dist",
-  mode: 'universal',
-  generate: {
-    dir: 'my-dist',
-    subFolders: false
-  },
-  robots: {
-    UserAgent: '*',
-    Disallow: '/'
-  },
+  // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     htmlAttrs: {
       lang: 'ru'
     },
-    title: 'Nuxt APP',
+    title: 'online school',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -34,11 +24,27 @@ module.exports = {
   loading: { color: '#ddd' },
   css: [
     './assets/sass/default.sass',
-    './assets/fonts/font-face.sass' // Path to your css file
   ],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [
+  ],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
-    // Doc: https://axios.nuxtjs.org/usage
+    // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+    // https://go.nuxtjs.dev/content
+    '@nuxt/content',
     'nuxt-trailingslash-module',
     'nuxt-webfontloader',
     'cookie-universal-nuxt',
@@ -47,199 +53,18 @@ module.exports = {
     '@nuxtjs/dotenv',
     'nuxt-font-loader-strategy'
   ],
-  sitemap: {
-    hostname: "http://localhost:3005",
-    default: {
-      changefreq: 'daily',
-      priority: 1,
-      lastmod: new Date()
-    },
-  },
 
-
-  generate: {
-    cache: {
-      ignore: [
-        // When something changed in the docs folder, do not re-build via webpack
-        'docs'
-      ]
-    }
-  },
   fontLoader: {
     url: '~~/assets/fonts/font-face.sass' // Path to your css file
   },
-  webfontloader: {
-    events: false,
-    google: {
-      families: ['Montserrat:400,500,600:cyrillic&display=swap']
-    },
-    timeout: 5000
-  },
-  styleResources: {
-    // your settings here
-    // scss: ['./assets/scss/global-variables.scss'], // alternative: scss
-    less: [],
-    stylus: []
-  },
-  /*
-  ** Axios module configuration
-  */
-  axios: {
-    // See https://github.com/nuxt-community/axios-module#options
-  },
-  render: {
-    // http2: {
-    //     push: true,
-    //     pushAssets: (req, res, publicPath, preloadFiles) => preloadFiles
-    //     .map(f => `<${publicPath}${f.file}>; rel=preload; as=${f.asType}`)
-    //   },
-    compressor: true,
-    resourceHints: false,
-    etag: true,
-    static: {
-      etag: true
-    }
-  },
-  /*
-  ** Build configuration
-  */
+  
+  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
+  axios: {},
+
+  // Content module configuration (https://go.nuxtjs.dev/config-content)
+  content: {},
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
-    optimizeCss: false,
-    robots: {
-      UserAgent: '*',
-      Disallow: '/'
-    },
-    filenames: {
-      app: ({ isDev }) => isDev ? '[name].js' : 'js/[contenthash].js',
-      chunk: ({ isDev }) => isDev ? '[name].js' : 'js/[contenthash].js',
-      css: ({ isDev }) => isDev ? '[name].css' : 'css/[contenthash].css',
-      img: ({ isDev }) => isDev ? '[path][name].[ext]' : 'img/[contenthash:7].[ext]',
-      font: ({ isDev }) => isDev ? '[path][name].[ext]' : 'fonts/[contenthash:7].[ext]',
-      video: ({ isDev }) => isDev ? '[path][name].[ext]' : 'videos/[contenthash:7].[ext]'
-    },
-    ...(!isDev && {
-      html: {
-        minify: {
-          collapseBooleanAttributes: true,
-          decodeEntities: true,
-          minifyCSS: true,
-          minifyJS: true,
-          processConditionalComments: true,
-          removeEmptyAttributes: true,
-          removeRedundantAttributes: true,
-          trimCustomFragments: true,
-          useShortDoctype: true
-        }
-      }
-    }),
-    splitChunks: {
-      layouts: true,
-      pages: true,
-      commons: true
-    },
-    optimization: {
-      minimize: !isDev
-    },
-    ...(!isDev && {
-      extractCSS: {
-        ignoreOrder: true
-      }
-    }),
-    transpile: ['vue-lazy-hydration', 'intersection-observer'],
-    postcss: {
-      plugins: {
-        ...(!isDev && {
-          cssnano: {
-            preset: ['advanced', {
-              autoprefixer: false,
-              cssDeclarationSorter: false,
-              zindex: false,
-              discardComments: {
-                removeAll: true
-              }
-            }]
-          }
-        })
-      },
-      ...(!isDev && {
-        preset: {
-          browsers: 'cover 99.5%',
-          autoprefixer: true
-        }
-      }),
-
-      order: 'cssnanoLast'
-    },
-    extend(config, ctx) {
-      const ORIGINAL_TEST = '/\\.(png|jpe?g|gif|svg|webp)$/i'
-      const vueSvgLoader = [
-        {
-          loader: 'vue-svg-loader',
-          options: {
-            svgo: false
-          }
-        }
-      ]
-      const imageMinPlugin = new ImageminPlugin({
-        pngquant: {
-          quality: '5-30',
-          speed: 7,
-          strip: true
-        },
-        jpegtran: {
-          progressive: true
-
-        },
-        gifsicle: {
-          interlaced: true
-        },
-        plugins: [
-          imageminMozjpeg({
-            quality: 70,
-            progressive: true
-          })
-
-        ]
-      })
-      if (!ctx.isDev) config.plugins.push(imageMinPlugin)
-
-      config.module.rules.forEach(rule => {
-        if (rule.test.toString() === ORIGINAL_TEST) {
-          rule.test = /\.(png|jpe?g|gif|webp)$/i
-          rule.use = [
-            {
-              loader: 'url-loader',
-              options: {
-                limit: 1000,
-                name: ctx.isDev ? '[path][name].[ext]' : 'img/[contenthash:7].[ext]'
-              }
-            }
-          ]
-        }
-      })
-      //  Create the custom SVG rule
-      const svgRule = {
-        test: /\.svg$/,
-        oneOf: [
-          {
-            resourceQuery: /inline/,
-            use: vueSvgLoader
-          },
-          {
-            resourceQuery: /data/,
-            loader: 'url-loader'
-          },
-          {
-            resourceQuery: /raw/,
-            loader: 'raw-loader'
-          },
-          {
-            loader: 'file-loader' // By default, always use file-loader
-          }
-        ]
-      }
-
-      config.module.rules.push(svgRule) // Actually add the rule
-    }
   }
 }
