4bda47cf0b09e8b3d012f51a715be0f6fdbcaf44
Serveyk0 <ssimpl8ss@gmail.com>
2021-01-16 19:41:07 +0200

    1

--- server/index.js
+++ server/index.js
@@ -18,6 +18,7 @@ app.use("/api/users", userRoutes);
 const mailRoutes = require("./routes/mail.js");
 app.use("/api/mail", mailRoutes);
 async function start() {
+  console.log(process.env)
   try {
     mongoose.connect(`mongodb+srv://${process.env.LOGIN}:${process.env.PWD}@${process.env.CLUSTER}.hvvrs.mongodb.net/${process.env.COLEECTION}?retryWrites=true&w=majority`, {
       useNewUrlParser: true,
