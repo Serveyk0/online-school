ff8d0631bb31f3bd0de1b60a0c3d8b1263f60ec0
Serveyk0 <ssimpl8ss@gmail.com>
2021-01-17 12:22:55 +0200

    1

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,14 @@
 module.exports = {
-  apps : [{
-      name      : 'online-school', // App name that shows in `pm2 ls`
-      exec_mode : 'cluster', // enables clustering
-      instances : 'max', // or an integer
-      script    : './node_modules/nuxt/bin/nuxt.js', // The magic key
-      args      : "start",
-  }]
-};
+  apps : [
+    {
+      name: "nuxt-dev",
+      script: "npm",
+      args: "run dev"
+    },
+    {
+      name: "nuxt-prod",
+      script: "npm",
+      args: "run start"
+    }
+  ]
+}
